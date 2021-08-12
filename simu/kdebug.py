c7faff9875a76cb7900b2075a8a346fd582b00a8
luciano kruk <sp04@kruk.eng.br>
2021-08-12 09:34:34 +0200

    migrating to python3

--- simu/kdebug.py
+++ simu/kdebug.py
@@ -16,38 +16,38 @@ class CKDEBUG:
 
     def show_matrix_details(self, name, D, level=10, linenb=0, fname=""):
         if (level <= self.maxlevel):
-            print "---------------------------"
+            print("---------------------------")
             if (linenb > 0):
-                print ": (at line %d)" % linenb
+                print(": (at line {:d})".format(linenb))
 
             if (len(fname) > 0):
-                print ": (%s)" % fname
+                print(": ({:s})".format(fname))
 
-            print ": %s <%s> =" % (name, type(D))
-            print D
+            print(": {:s} <{:s}> =".format(name, type(D).__name__))
+            print(D)
 
             if isinstance(D, np.ndarray):
-                print ": shape ="
-                print D.shape
+                print(": shape =")
+                print(D.shape)
 
     def linenb(self):
         """Returns the current line number in our program."""
         return inspect.currentframe().f_back.f_lineno
 
     def show_traceback(self):
-        print traceback.print_stack()
+        print(traceback.print_stack())
 
     def abort(self):
-        print "============================"
-        print "CKDEBUG:   aborting... "
-        print "============================"
+        print("============================")
+        print("CKDEBUG:   aborting... ")
+        print("============================")
         sys.exit(-1)
 
     def assert_infnan(self, np_array, txt):
         if (np.any(np.isnan(np_array)) or np.any(np.isinf(np_array))):
-            print
-            self.show_matrix_details("array with nan (%s) =" % txt, np_array, level=0)
-            print
+            print()
+            self.show_matrix_details("array with nan ({:s}) =".format(txt), np_array, level=0)
+            print()
             self.show_traceback()
             self.abort();
 
@@ -56,17 +56,17 @@ class CKDEBUG:
 if (__name__ == "__main__"):
     kd = CKDEBUG(True)
     kd.show_matrix_details("zeros", np.zeros((2,2)))
-    print "current line = %d" % kd.linenb()
+    print("current line = {:d}".format(kd.linenb()))
 
     kd = CKDEBUG(False)
     kd.show_matrix_details("zeros", np.zeros((2,2)))
-    print "current line = %d" % kd.linenb()
+    print("current line = {:d}".format(kd.linenb()))
 
     kd = CKDEBUG(True)
     kd.assert_infnan(0, "line 1")
     kd.assert_infnan(np.nan + np.zeros((3,3)), "line 2")
 
     # it is not supposed to get here...
-    print "ops..."
+    print("ops...")
 
 #==//==//==//==//==//==//==//==//==//==//==//==//==//==//==//==//==//==//==//==#
